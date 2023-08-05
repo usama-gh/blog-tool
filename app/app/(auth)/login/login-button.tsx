c77a3201cdac688bcbb87ec3e1f1166e0aad4130
ssidharth010 <ssidharth010@gmail.com>
2023-08-05 13:03:54 +0530

    Fix: Google auth issue

--- app/app/(auth)/login/login-button.tsx
+++ app/app/(auth)/login/login-button.tsx
@@ -23,7 +23,7 @@ export default function LoginButton() {
       disabled={loading}
       onClick={() => {
         setLoading(true);
-        signIn("github");
+        signIn("google");
       }}
       className={`${
         loading
