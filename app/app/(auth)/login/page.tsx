fab02d640fc9db41a1a0990ec3f29adb0353fedf
usama <ukhalid639@gmail.com>
2023-09-27 20:48:28 +0500

    fix

--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -10,11 +10,11 @@ export default function LoginPage() {
         alt="SlidesBites social blog builder"
         width={800}
         height={800}
-        className="absolute left-1/2 transform -translate-x-1/2 -z-10 top-5 left-0 mx-auto w-auto dark:scale-110 dark:rounded-full dark:border dark:border-gray-400"
+        className="absolute left-1/2 transform -translate-x-1/2 -z-10 top-8 left-0 mx-auto w-auto dark:scale-110 "
         src="/login-visual.png"
       />
 
-<div className="relative mx-5 bg-white rounded-xl shadow-xl shadow-gray-200 py-10 dark:border-gray-700 sm:mx-auto sm:w-full sm:max-w-md ">
+<div className="relative mx-5 bg-white dark:bg-gray-700 dark:shadow-gray-800 rounded-xl shadow-xl shadow-gray-200 py-10 dark:border-gray-700 sm:mx-auto sm:w-full sm:max-w-md ">
 
 
 <Image
