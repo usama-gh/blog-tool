9cb5646c0089016a64edead7d07b30eb419bec4c
usama <ukhalid639@gmail.com>
2023-10-21 20:11:10 +0500

    testddd

--- app/[domain]/[slug]/opengraph-image.tsx
+++ app/[domain]/[slug]/opengraph-image.tsx
@@ -45,7 +45,7 @@ export default async function PostOG({
             />
           </div>
         ) : (
-          <div tw="flex flex-col bg-gradient-to-r from-slate-900 to-slate-700 justify-center items-start relative bg-slate-800  px-24 h-full w-full">
+          <div tw="flex flex-col bg-gradient-to-r from-slate-900 to-slate-700 justify-center items-start relative bg-slate-800  px-24 h-full w-full min-h-screen ">
             <span tw="absolute right-5 top-5  scale-50">
               <svg  width="153" height="153" viewBox="0 0 153 153" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M125.278 135.432C157.825 108.493 162.372 60.27 135.433 27.7226C108.494 -4.82479 60.2704 -9.37128 27.723 17.5677C-4.8244 44.5068 -9.37089 92.73 17.5681 125.277C44.5072 157.825 92.7304 162.371 125.278 135.432ZM54.5864 51.7648C51.1498 51.4408 48.1013 53.9641 47.7773 57.4006C47.4533 60.8371 49.9765 63.8857 53.4131 64.2097L86.9095 67.3677L83.7514 100.864C83.4274 104.301 85.9507 107.349 89.3872 107.673C92.8237 107.997 95.8723 105.474 96.1963 102.037L99.9409 62.3186C100.265 58.882 97.7417 55.8335 94.3052 55.5095L54.5864 51.7648Z" fill="#FBE54F" />
