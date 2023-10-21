80e364cc48214d0b08197d8ba94aa428a6fc1c31
usama <ukhalid639@gmail.com>
2023-10-21 23:38:39 +0500

    new-paddle

--- components/PaddleLoader.tsx
+++ components/PaddleLoader.tsx
@@ -17,10 +17,10 @@ export function PaddleLoader({ subscriptionId, userId }: Props) {
       //   src="https://cdn.paddle.com/paddle/paddle.js"
       src="https://cdn.paddle.com/paddle/v2/paddle.js"
       onLoad={() => {
-        if (process.env.NEXT_PUBLIC_PADDLE_SANDBOX) {
-          // @ts-ignore
-          Paddle.Environment.set("sandbox");
-        }
+        // if (process.env.NEXT_PUBLIC_PADDLE_SANDBOX) {
+        //   // @ts-ignore
+        //   Paddle.Environment.set("sandbox");
+        // }
         // @ts-ignore
         Paddle.Setup({
           seller: Number(process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID),
