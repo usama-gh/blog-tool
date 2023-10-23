13283ff4fe5f0bd1e0704d9864601e25a0084d18
usama <ukhalid639@gmail.com>
2023-10-23 18:29:35 +0500

    fix

--- app/[domain]/[slug]/opengraph-image.tsx
+++ app/[domain]/[slug]/opengraph-image.tsx
@@ -44,7 +44,7 @@ export default async function PostOG({
   return new ImageResponse(
     (
       
-        data.image !== "https://ige9ec25vizexnyy.public.blob.vercel-storage.com/WFClpH0wjj2gu1mBXIlms-MnS5yKErN4UsYCqCMs4tezoQ6G2IXS.png" ? (
+        data.image !== "https://ige9ec25vizexnyy.public.blob.vercel-storage.com/tQidqrn-Z2Nd3W4IJXq3XBZZMOllnA06WIkhlq.jpeg" ? (
           <div tw="flex flex-col justify-center items-center w-full h-screen">
             <img
               tw="w-full"
