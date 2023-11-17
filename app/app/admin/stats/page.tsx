76544695bb7653f77278428092b9279baec8b089
usmanlatif306 <usmanlatif603@gmail.com>
2023-11-17 20:42:54 +0500

    admin blog users,sites,posts total state

--- app/app/admin/stats/page.tsx
+++ app/app/admin/stats/page.tsx
@@ -2,6 +2,7 @@ import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
 import BlogStats from "@/components/blog-stats";
+import { Globe, Globe2, Users } from "lucide-react";
 
 export default async function Stats() {
   const session = await getSession();
@@ -36,6 +37,9 @@ export default async function Stats() {
     return b.views[0].views - a.views[0].views;
   });
 
+  const users = await prisma.user.count();
+  const posts = await prisma.post.count();
+
   return (
     <>
       <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
@@ -43,6 +47,38 @@ export default async function Stats() {
           <h1 className="font-inter hide_onboarding text-3xl font-bold dark:text-white">
             Blog Stats
           </h1>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className="flex flex-col items-center gap-2 rounded-xl border bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-slate-900 dark:shadow-slate-700/[.7] md:p-5">
+            <Users width={18} className="h-10 w-10 text-blue-500" />
+            <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
+              {users}
+            </h3>
+            <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+              Registered Users
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center gap-2 rounded-xl border bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-slate-900 dark:shadow-slate-700/[.7] md:p-5">
+            <Globe width={18} className="h-10 w-10 text-blue-500" />
+            <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
+              {blogs.length}
+            </h3>
+            <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+              Sites Created
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center gap-2 rounded-xl border bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-slate-900 dark:shadow-slate-700/[.7] md:p-5">
+            <Globe2 width={18} className="h-10 w-10 text-blue-500" />
+            <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
+              {posts}
+            </h3>
+            <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
+              Posts Created
+            </p>
+          </div>
         </div>
 
         <div className="flex flex-col space-y-6">
