cb98159f32597e8528209a8f452e41d8c0ac33b4
usama <ukhalid639@gmail.com>
2023-12-16 19:57:25 +0500

    fix

--- components/lead-download.tsx
+++ components/lead-download.tsx
@@ -56,7 +56,7 @@ export const LeadDownload = ({
           </p>
         ) : (
           <div className="mt-5 text-center">
-            <p className="text-sm text-slate-700 dark:text-gray-200 text-center flex justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <p className="text-sm text-slate-700 dark:text-gray-200 text-center flex justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
 </svg>
 </p>
