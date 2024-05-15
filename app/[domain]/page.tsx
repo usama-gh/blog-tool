5c0787a83a719b05d9e0eed446aed449150068ec
usama <ukhalid639@gmail.com>
2024-05-15 22:35:03 +0500

    some design

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import { notFound } from "next/navigation";
 import BlurImage from "@/components/blur-image";
 import {
@@ -33,16 +34,19 @@ export default async function SiteHomePage({
     <>
       <div className="mx-auto max-w-6xl">
         <div className="ease left-0 right-0 top-0 z-30 flex w-full transition-all duration-150 dark:bg-gray-800 dark:text-white">
-          <div className="py-12">
-            <div className="flex items-center gap-3">
-              <div className="flex w-full items-center justify-center gap-3">
-                <div className="h-24 w-60 rounded">
+          <div className="py-12 mx-auto max-w-3xl w-full text-center">
+            <div>
+              <div className="w-full flex flex-col items-center text-center justify-center gap-3">
+                
+             
+
+                <div className=" text-center">
                   {data.logo ? (
                     <Image
                       alt={data.user?.name ?? "User Avatar"}
-                      width={100}
-                      height={100}
-                      className="object-cover"
+                      width={80}
+                      height={80}
+                      className="object-cover rounded-2xl mb-3"
                       src={data.logo}
                     />
                   ) : (
@@ -51,31 +55,128 @@ export default async function SiteHomePage({
                     </div>
                   )}
                 </div>
-                <div className="">
+
                   <h1 className="bg-gradient-to-br from-slate-600 to-slate-600 bg-clip-text text-lg font-bold text-transparent dark:from-gray-50 dark:to-gray-500  dark:drop-shadow-md">
                     {data.name}
                   </h1>
                   {data?.bio && (
-                    <div className="font-regular site-bio text-md overflow-hidden">
+                    <div className="font-regular site-bio text-md text-center overflow-hidden">
                       {parse(data.bio)}
                     </div>
                   )}
 
                   <SocialLinks linksData={data.links} />
-                </div>
+              
               </div>
               {/* susbcribe to blog */}
-              <Subscribe siteId={data.id} />
+              <div className="w-1/4 ">
+              {/* <Subscribe siteId={data.id} /> */}
+              </div>
+             
             </div>
           </div>
         </div>
 
-        <h3 className="mb-5 text-lg font-semibold tracking-tight  text-slate-600 dark:text-white md:text-xl">
-          Latest Blog
-        </h3>
+        <div className="flex items-start gap-3">
+        <div className="w-1/4">
+          <div className="">
+        
+                <Subscribe siteId={data.id} view="homepage" />
+                </div>
+                </div>
+              <div className="w-3/4">
+
+
+
+
+
+
+              {posts.length > 0 ? (
+          <div className="mx-auto w-full max-w-screen-xl md:mb-28 lg:w-5/6">
+
+<div>
+                <Link href={`/${posts[0].slug}`}>
+                  <div
+                    className="ease mb-5 rounded-3xl border border-slate-200 px-4 py-8 transition-all hover:border-slate-300 dark:overflow-hidden dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:hover:border-gray-600 md:w-full"
+                    style={{
+                      backgroundImage: `url(${
+                        !isDefaultImage(posts[0].image) ? posts[0].image : ""
+                      })`,
+                      backgroundPosition: "center",
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  >
+                    <div className="m-auto flex w-full flex-col items-center justify-center gap-y-2 px-8 text-center">
+                      <p
+                        className={cn(
+                          "m-auto text-sm tracking-normal text-slate-500 dark:text-gray-400",
+                          !isDefaultImage(posts[0].image) && "text-white",
+                        )}
+                      >
+                        {toDateString(posts[0].createdAt, "long")}
+                      </p>
+                      <h2
+                        className={cn(
+                          "text-2xl font-semibold tracking-tight text-slate-600  dark:text-white md:text-3xl",
+                          !isDefaultImage(posts[0].image) && "text-white",
+                        )}
+                      >
+                        {posts[0].title}
+                      </h2>
+                      <p
+                        className={cn(
+                          "line-clamp-3 w-full text-base leading-6 text-slate-500 dark:text-gray-300",
+                          !isDefaultImage(posts[0].image) && "text-white",
+                        )}
+                      >
+                        {posts[0].description}
+                      </p>
+                      <button
+                        className={cn(
+                          "mt-2 w-auto rounded-full border border-slate-500 px-4 py-2 text-center text-sm text-slate-500 hover:border-slate-700 hover:text-slate-600 dark:border-gray-600 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-300 dark:hover:text-gray-300",
+                          !isDefaultImage(posts[0].image) &&
+                            "border-white bg-white text-black",
+                        )}
+                      >
+                        Read More
+                      </button>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+
+
+
+            
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center py-20">
+            <Image
+              alt="missing post"
+              src="https://illustrations.popsy.co/gray/success.svg"
+              width={400}
+              height={400}
+              className="dark:hidden"
+            />
+            <Image
+              alt="missing post"
+              src="https://illustrations.popsy.co/white/success.svg"
+              width={400}
+              height={400}
+              className="hidden dark:block"
+            />
+            <p className="font-title text-2xl text-stone-600 dark:text-stone-400">
+              No posts yet.
+            </p>
+          </div>
+        )}
+      
+
+
         <div className="grid grid-cols-1 gap-4 px-2 pb-16 md:grid-cols-2">
           {posts.length > 0 ? (
-            posts.map((post, index) => (
+            posts.slice(1).map((post, index) => (
               <div key={`post-${index}`}>
                 <Link href={`/${post.slug}`}>
                   <div
@@ -150,6 +251,20 @@ export default async function SiteHomePage({
             </div>
           )}
         </div>
+
+
+
+
+
+
+
+
+                </div>
+                
+    </div>
+
+
+      
 
         {leads.length > 0 && (
           <>
--- components/subscribe.tsx
+++ components/subscribe.tsx
@@ -3,12 +3,14 @@
 import { SubscribeReponse } from "@/types";
 import LoadingDots from "./icons/loading-dots";
 import { cn } from "@/lib/utils";
+import {Mail} from "lucide-react"
 import { useState } from "react";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 import { toast } from "sonner";
 
-export const Subscribe = ({ siteId }: { siteId: string }) => {
+export const Subscribe = ({ siteId,view }: { siteId: string; view: string }) => {
   const [email, setEmail] = useState("");
+  
   const [isSubscribed, setIsSubscribed] = useState(false); // New state variable
 
   const addToSubscribe = async (e: any) => {
@@ -35,61 +37,145 @@ export const Subscribe = ({ siteId }: { siteId: string }) => {
 
   return (
     <div className="mx-auto max-w-lg">
-      {/* <div className="mx-auto h-[2px] w-8 bg-slate-300 dark:bg-gray-600"></div> */}
-      <div className="relative mx-auto w-full rounded-2xl  px-8 py-4 text-center text-slate-400 dark:text-gray-400">
-        {isSubscribed ? (
-          // Display both success messages
-          <>
-            <p className="mb-1 font-semibold text-slate-800 dark:text-gray-300">
-              Thank you for subscribing!
-            </p>
-            <p className="text-sm text-slate-800 dark:text-gray-300">
-              You are now subscribed to the newsletter.
-            </p>
-          </>
-        ) : (
-          // Display the form
-          <>
-            <p className="text-sm text-slate-500 dark:border-gray-600 dark:text-gray-400">
-              Subscribe to get future posts, exclusive content & much more.
-            </p>
 
-            <form
-              onSubmit={addToSubscribe}
-              className="mt-3 flex items-center gap-x-1"
-            >
-              <input
-                name="name"
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className="w-full flex-1 rounded-md border border-slate-200 bg-slate-50 px-4 py-2 text-sm text-slate-600 placeholder:text-slate-400 focus:border-blue-400 focus:outline-none focus:ring-blue-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-300 dark:focus:ring-white"
-              />
-              <SubscribeButton />
-            </form>
-          </>
-        )}
-      </div>
+{view==="homepage" ? (
+
+
+<div className="relative mx-auto text-left  bg-teal-100 rounded-2xl p-6 w-full">
+{isSubscribed ? (
+  // Display both success messages
+  <>
+    <p className="mb-1 font-semibold  dark:text-gray-300">
+      Thank you for subscribing!
+    </p>
+    <p className="text-sm dark:text-gray-300">
+      You are now subscribed to the newsletter.
+    </p>
+  </>
+) : (
+  // Display the form
+  <>
+
+
+<Mail size={'2.5rem'} strokeWidth={0.9} className="mb-2 text-teal-700"/>
+
+    <p className="text-base text-teal-700 ">
+      Subscribe to get future posts, exclusive content & much more.
+    </p>
+
+    <form
+      onSubmit={addToSubscribe}
+      className="mt-5 flex items-center"
+    >
+      <input
+        name="name"
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        className="w-full flex-1 shadow-md rounded-l-md bg-white border-0 text-xs"
+      />
+      <SubscribeButton view={view}/>
+    </form>
+  </>
+)}
+</div>
+
+
+
+) : (
+
+  <div className="relative mx-auto w-full rounded-2xl  px-8 py-4 text-center">
+  {isSubscribed ? (
+    // Display both success messages
+    <>
+      <p className="mb-1 font-semibold  dark:text-gray-300">
+        Thank you for subscribing!
+      </p>
+      <p className="text-sm dark:text-gray-300">
+        You are now subscribed to the newsletter.
+      </p>
+    </>
+  ) : (
+    // Display the form
+    <>
+      <p className="text-sm ">
+        Subscribe to get future posts, exclusive content & much more.
+      </p>
+
+      <form
+        onSubmit={addToSubscribe}
+        className="mt-3 flex items-center gap-x-1"
+      >
+        <input
+          name="name"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="w-full flex-1 rounded-md border border-slate-200 bg-slate-50 px-4 py-2 text-sm text-slate-600 placeholder:text-slate-400 focus:border-blue-400 focus:outline-none focus:ring-blue-400 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-300 dark:focus:ring-white"
+        />
+        <SubscribeButton view={view} />
+      </form>
+    </>
+  )}
+</div>
+
+)}
+     
+   
+     
     </div>
   );
 };
 
-function SubscribeButton() {
+function SubscribeButton({view}) {
   const { pending } = useFormStatus();
   return (
+    
+    <div>
+
+{view==="homepage" ? (
+     
+
     <button
-      type="submit"
-      className={cn(
-        "flex h-10 w-auto items-center justify-center space-x-2 rounded-md border px-4 py-1 text-sm transition-all focus:outline-none",
-        pending
-          ? "cursor-not-allowed border-slate-200 bg-slate-100 text-slate-400 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300"
-          : "border-0 bg-slate-200 text-slate-600 hover:bg-slate-300 hover:text-slate-800 dark:border dark:border-black dark:border-gray-700 dark:bg-transparent dark:text-gray-200 dark:hover:border-gray-400 dark:hover:bg-transparent dark:hover:text-white dark:active:bg-gray-800",
-      )}
-      disabled={pending}
-    >
-      {pending ? <LoadingDots color="#808080" /> : <p>Subscribe</p>}
-    </button>
+    type="submit"
+    className={cn(
+      "flex h-8 w-auto items-center shadow-md justify-center space-x-2 rounded-r-md  text-white px-4 py-1 text-xs transition-all focus:outline-none",
+      pending
+        ? "cursor-not-allowed "
+        : "border-0 bg-teal-600",
+    )}
+    disabled={pending}
+  >
+    {pending ? <LoadingDots color="#808080" /> : <p>Subscribe</p>}
+  </button>
+
+
+    ):(
+     
+
+
+    <button
+    type="submit"
+    className={cn(
+      "flex h-10 w-auto items-center justify-center space-x-2 rounded-md border px-4 py-1 text-sm transition-all focus:outline-none",
+      pending
+        ? "cursor-not-allowed border-slate-200 bg-slate-100 text-slate-400 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300"
+        : "border-0 bg-slate-200 text-slate-600 hover:bg-slate-300 hover:text-slate-800 dark:border dark:border-black dark:border-gray-700 dark:bg-transparent dark:text-gray-200 dark:hover:border-gray-400 dark:hover:bg-transparent dark:hover:text-white dark:active:bg-gray-800",
+    )}
+    disabled={pending}
+  >
+    {pending ? <LoadingDots color="#808080" /> : <p>Subscribe</p>}
+  </button>
+
+
+    )}
+  
+    </div>
+   
+
   );
 }
