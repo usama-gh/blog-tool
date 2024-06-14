86c0c046e50343443175c6cc30a4dcd76fad7574
Muhammad Usman Latif <usmanlatif603@gmail.com>
2024-06-14 13:10:02 +0500

    dark mode toggle added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { cal, inter } from "@/styles/fonts";
 import { Providers } from "./providers";
 import { Metadata } from "next";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const title =
   "Typedd – Turn your social media posts into blogs & create a personal social blog";
@@ -36,8 +37,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={'overflow-y-auto tracking-tight min-h-screen h-full bg-gradient-to-b from-white to-[#F4F8FF] dark:bg-gray-800 dark:bg-none '+cn(cal.variable, inter.variable)}>
-        <Providers>{children}</Providers>
+      <body
+        className={
+          "h-full min-h-screen overflow-y-auto bg-gradient-to-b from-white to-[#F4F8FF] tracking-tight dark:bg-gray-800 dark:bg-none " +
+          cn(cal.variable, inter.variable)
+        }
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Providers>{children}</Providers>
+        </ThemeProvider>
       </body>
     </html>
   );
