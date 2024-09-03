5bdc81527703f1df4f48c4da4c0e7c2ba404aceb
usama <ukhalid639@gmail.com>
2024-09-03 14:07:25 +0500

    fix

--- app/app/(dashboard)/layout.tsx
+++ app/app/(dashboard)/layout.tsx
@@ -1,10 +1,18 @@
 import { ReactNode, Suspense } from "react";
 import Profile from "@/components/profile";
 import Nav from "@/components/nav";
+import Script from "next/script";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div>
+        <Script
+        src="https://assets.userscom.com/script.js"
+        id="userscom-chat"
+        data-reference="6553c9324bb5c"
+        strategy="lazyOnload" // Load script after the page is loaded
+        onLoad={() => console.log('Script loaded successfully')}
+      />
       <Nav>
         <Suspense fallback={<div>Loading...</div>}>
           <Profile />
