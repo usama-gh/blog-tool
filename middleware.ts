66b0e59384a412855a095a962c06138860c9eec8
usama <ukhalid639@gmail.com>
2024-09-15 22:46:33 +0500

    Fix

--- middleware.ts
+++ middleware.ts
@@ -3,52 +3,57 @@ import { getToken } from "next-auth/jwt";
 
 export const config = {
   matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+    // "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
 };
 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
+
+  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   const hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
-  // Basic bot detection
-  const userAgent = req.headers.get("user-agent") || "";
-  const isBot = /bot|crawl|slurp|spider|robot|crawling/i.test(userAgent);
-  
-  if (isBot) {
-    return NextResponse.rewrite(new URL(`/bots${url.pathname}`, req.url));
-  }
+  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+  const path = url.pathname;
 
-  // Rewrites for app pages
+  // rewrites for app pages
   if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
     const session = await getToken({ req });
-    if (!session && url.pathname !== "/login") {
+    if (!session && path !== "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
-    } else if (session && url.pathname == "/login") {
+    } else if (session && path == "/login") {
       return NextResponse.redirect(new URL("/", req.url));
     }
     return NextResponse.rewrite(
-      new URL(`/app${url.pathname === "/" ? "" : url.pathname}`, req.url),
+      new URL(`/app${path === "/" ? "" : path}`, req.url),
     );
   }
 
-  // Rewrite root application to `/home` folder
+  // rewrite root application to `/home` folder
   if (
     hostname === "localhost:3002" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
-    return NextResponse.rewrite(new URL(`/home${url.pathname}`, req.url));
+    return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
-  // Special case for `vercel.pub` domain
+  // special case for `vercel.pub` domain
   if (hostname === "vercel.pub") {
     return NextResponse.redirect(
       "https://vercel.com/blog/platforms-starter-kit",
     );
   }
 
-  // Rewrite everything else to `/[domain]/[path]` dynamic route
-  return NextResponse.rewrite(new URL(`/${hostname}${url.pathname}`, req.url));
+  // rewrite everything else to `/[domain]/[path] dynamic route
+  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
