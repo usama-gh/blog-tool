07360ac2d5693556433a2eb9e2a32106113d88e0
usama <ukhalid639@gmail.com>
2024-10-05 01:33:31 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -126,15 +126,7 @@ module.exports = {
           },
         ],
       },
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Robots-Tag',
-            value: 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
-          },
-        ],
-      },
+      
     ];
     
   },
