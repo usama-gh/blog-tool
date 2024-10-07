50d72df198886f6b28f11bc6ae7b2f01cc62aaeb
usama <ukhalid639@gmail.com>
2024-10-07 19:11:46 +0500

    fix

--- app/robots.tsx
+++ app/robots.tsx
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/'
+      allow: '*',
     },
     sitemap: `${protocol}://${domain}/sitemap.xml`,
   }
